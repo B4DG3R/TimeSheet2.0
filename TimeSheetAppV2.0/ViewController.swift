5ba73126f67df8c666bcf14c0cc339a9a339e39a
Matthew Hollyhead <matt.hollyhead@live.com>
2020-06-14 07:17:13 +0100

    Initial Commit

---
+++ TimeSheetAppV2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeSheetAppV2.0
+//
+//  Created by Matthew Hollyhead on 14/06/2020.
+//  Copyright © 2020 Matthew Hollyhead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
